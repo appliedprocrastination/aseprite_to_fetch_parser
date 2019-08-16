48537c39e3f913802accaa2501c5a3af1bfec952
Amund Grorud <amund@grorud.net>
2019-08-16 19:14:20 +0200

    a quick bug fix (dont know what i did) and added autosave to SD card (hard coded)

--- parser.py
+++ parser.py
@@ -29,21 +29,27 @@ def aseprite_to_numpy(file_path):
     #Check if there are used multiple layers
     if frames[0].num_chunks == 4:
         layers = False
-    elif frames[0].num_chunks == 6:
+    elif frames[0].num_chunks >= 6:
         layers = True
     else:
         raise ValueError("Unkonwn number of chunks in first frame: {}".format(frames[0].num_chunks))
     
     width, height = header.width, header.height
     animation = []
+    new_frame = False
+    frame = None
     for i, frame in enumerate(frames):
+        print("Frame:",i)
         for chunk in frame.chunks:
             # Only insterested in the chunks containing the data
             if chunk.chunk_type == 0x2005:
+                print("Data Chunck!")
                 # Make sure to use the right layer index dependent on wether multiple layers are used or not
-                if (layers and chunk.layer_index == 1) or (not layers and chunk.layer_index == 0):
+                if (layers and chunk.layer_index) or (not layers and chunk.layer_index == 0):
+                    if not new_frame:
+                        frame = np.zeros((width, height))
+                        new_frame = True
                     chunk_width, chunk_height = int(chunk.data["width"]), int(chunk.data["height"])
-                    frame = np.zeros((width, height), dtype="uint8")
                     data = binascii.hexlify(chunk.data["data"])
                     cnt_x = chunk.x_pos
                     cnt_y = chunk.y_pos
@@ -56,19 +62,32 @@ def aseprite_to_numpy(file_path):
                         #If grayscale are used, flip the scale
                         if color_size == 4:
                             val_int = 255 - val_int
-                        frame[cnt_x, cnt_y] = val_int
+                        if val_int > 0:
+                            frame[cnt_x, cnt_y] = val_int
                         cnt_x += 1
                         if cnt_x == chunk_width + chunk.x_pos:
                             cnt_x  = chunk.x_pos
                             cnt_y += 1
-                    animation.append(frame)
+        if new_frame:   
+            animation.append(frame)
+            new_frame = False
+    for i, a in enumerate(animation):
+        print("Frame:",i)
+        print(a)
+        print()
+        #print(np.flip(a.transpose()))
+        #print()
+    print(len(frames))
+    print(len(animation))
+    
+
     return animation
 
 def numpy_to_fetch(animation, outfile="out", scale_from=1, scale_to=1):
     num_frames = len(animation)
     num_x, num_y = animation[0].shape
     pwm_frames = []
-    with open("{}.bin".format(outfile), "wb") as fp:
+    with open("{}_D.bin".format(outfile), "wb") as fp:
         for i in range(num_frames):
             #Dont want to change original frame, better safe then sorry
             frame = cp.deepcopy(animation[i])
@@ -87,7 +106,7 @@ def numpy_to_fetch(animation, outfile="out", scale_from=1, scale_to=1):
             frame[frame == 0] = 20
 
             #If svaling are set, this will take care of it
-            pwm_frame_uint8 = np.uint8(frame) * scale_to / scale_from
+            pwm_frame_uint8 = np.uint8(frame)# * scale_to / scale_from
             
             fp.write(bytes(binary_frame_array_uint32))
             pwm_frames.append(pwm_frame_uint8)
@@ -96,13 +115,16 @@ def numpy_to_fetch(animation, outfile="out", scale_from=1, scale_to=1):
             fp.write(bytes(pwm_frame))
 
     #Creates the config file
-    with open("{}.txt".format(outfile), "w") as fp:
+    with open("{}_C.txt".format(outfile), "w") as fp:
         fp.write("{},{},{},0,0,1,0,-1,0,-1,0".format(num_x, num_y, num_frames))
 
 if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Converts the aseprite file format to Fetch readable binary animation file.')
     import sys
-    if len(sys.argv) < 2:
-        print("No file path given...")
+    if len(sys.argv) < 3:
+        print("No file ...")
     else:
         animation_np = aseprite_to_numpy(sys.argv[1])
-        numpy_to_fetch(animation_np)
+        numpy_to_fetch(animation_np, outfile="/run/media/alphaos/3333-3438/"+sys.argv[2])
